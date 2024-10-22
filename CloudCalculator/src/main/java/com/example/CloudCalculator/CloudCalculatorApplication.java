c948d625917f763b6d46abfe8a00e693c4ff3099
Violeta Bahtijari <vbahtijari@stud.hs-heilbronn.de>
2024-10-22 10:15:35 +0200

    gitignore added

---
+++ CloudCalculator/src/main/java/com/example/CloudCalculator/CloudCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.CloudCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudCalculatorApplication.class, args);
+	}
+
+}
---
+++ CloudCalculator/src/test/java/com/example/CloudCalculator/CloudCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CloudCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
